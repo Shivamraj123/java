dd82ef2382f2a707ae07d66ca3bb595e6dd69fc6
Shivamraj123 <53595417+Shivamraj123@users.noreply.github.com>
2020-01-18 18:27:00 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Welcome to shivam, suhas and javed");
+      System.out.println("Hello all, Welcome to shivam raj, suhas and javed");
       System.out.println("Automatic build");
       System.out.println("Using PollSCM ");
    }
